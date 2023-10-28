4b37532ed2acfdeb6ab79f28597dc5b51b38b409
sagacity0321 <citronica0321@gmail.com>
2023-10-28 17:43:51 +0900

    Initial commit + start databinding

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnLogin.setOnClickListener{
+            val inputEmail = binding.etId.text.toString()
+            val inputPw = binding.etPw.text.toString()
+
+            
+        }
+    }
+}
